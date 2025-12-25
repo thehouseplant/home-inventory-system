207b9c152c6a589887002f83bd010b413d1c166f
Sean Collins <sean.collins@outlook.com>
2025-12-24 21:50:30 -0800

    Add dotenv, pg, and prisma-pg adapter to the project

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const prisma = new PrismaClient();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
